b562bf1edefa669d318c6375cea58c3b088776b7
Евгений Воронцов <sejeenn@gmail.com>
2024-11-05 16:22:27 +0300

    Добавлен файл main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from core.config import settings
+from core.models import db_helper
+from api import router as api_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
